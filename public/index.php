1269a115f430d28e652126c6c80559843463c048
Antonio Pontes <anvictorsantos@gmail.com>
2022-06-05 17:26:42 -0300

    fix - routes

--- public/index.php
+++ public/index.php
@@ -5,25 +5,19 @@ require __DIR__ . '/../vendor/autoload.php';
 use Alura\Cursos\Controller\ListarCursos;
 use Alura\Cursos\Controller\Persistencia;
 use Alura\Cursos\Controller\FormularioInsercao;
+use Alura\Cursos\Controller\InterfaceControladorRequisicao;
 
-switch ($_SERVER['PATH_INFO']) {
-    case '/listar-cursos':
-        $controlador = new ListarCursos();
-        $controlador->processaRequisicao();
-        break;
+$caminho = $_SERVER['PATH_INFO'];
+$rotas = require __DIR__ . '/../config/routes.php';
 
-    case '/novo-curso':
-        $controlador = new FormularioInsercao();
-        $controlador->processaRequisicao();
-        break;
-        break;
-
-    case '/salvar-curso':
-        $controlador = new Persistencia();
-        $controlador->processaRequisicao();
-        break;
-
-    default:
-        echo "Error 404";
-        break;
+if (!array_key_exists($caminho, $rotas)) {
+    http_response_code(404);
+    exit();
 }
+
+$classeControladora = $rotas[$caminho];
+/**
+ * @var InterfaceControladorRequisicao $controlado
+ */
+$controlador = new $classeControladora();
+$controlador->processaRequisicao();
