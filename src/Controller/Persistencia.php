c6b492ac1c6dab4a8a23d586b1d7e854182cc8bb
Antonio Pontes <anvictorsantos@gmail.com>
2022-06-06 09:36:02 -0300

    fix - editing and including a new record

--- src/Controller/Persistencia.php
+++ src/Controller/Persistencia.php
@@ -25,7 +25,19 @@ class Persistencia implements InterfaceControladorRequisicao
 
         $curso = new Curso();
         $curso->setDescricao($descricao);
-        $this->entityManager->persist($curso);
+
+        $id = filter_input(
+            INPUT_GET,
+            'id',
+            FILTER_VALIDATE_INT
+        );
+
+        if (!is_null($id)) {
+            $curso->setId($id);
+            $this->entityManager->merge($curso);
+        } else {
+            $this->entityManager->persist($curso);
+        }
         $this->entityManager->flush();
 
         header('Location: /listar-cursos', true, 302);
