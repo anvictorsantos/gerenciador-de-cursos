1a1357a1e762753993038a7438b65c8e7ed81edb
Antonio Pontes <anvictorsantos@gmail.com>
2022-06-07 21:06:22 -0300

    new - login session

--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,8 @@
 use Alura\Cursos\Controller\Exclusao;
 use Alura\Cursos\Controller\ListarCursos;
 use Alura\Cursos\Controller\Persistencia;
+use Alura\Cursos\Controller\RealizarLogin;
+use Alura\Cursos\Controller\FormularioLogin;
 use Alura\Cursos\Controller\FormularioEdicao;
 use Alura\Cursos\Controller\FormularioInsercao;
 
@@ -13,6 +15,8 @@ $rotas = [
     '/salvar-curso' => Persistencia::class,
     '/excluir-curso' => Exclusao::class,
     '/alterar-curso' => FormularioEdicao::class,
+    '/login' => FormularioLogin::class,
+    '/realiza-login' => RealizarLogin::class
 ];
 
 return $rotas;
